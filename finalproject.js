d18560508826d8d1f80da1ce20f1990f54ccfa89
Pearlynne <chongpearlynnelh@gmail.com>
2023-12-07 16:10:07 -0800

    Changed setItem type in localStorage from innerHTML to JSON array

--- finalproject.js
+++ finalproject.js
@@ -1,20 +1,65 @@
-// Display incompleted tasks from local storageto table
-document.getElementById("incomplete-tasks").innerHTML = localStorage.getItem("incomplete-tasks");
-
-// Save only complete and incomplete lists to the local storage
-function saveTDList() {
-	localStorage.setItem("incomplete-tasks", document.getElementById("incomplete-tasks").innerHTML)
-	localStorage.setItem("completed-tasks", document.getElementById("completed-tasks").innerHTML)
-}
-
 // Get form elements
 const form = document.getElementById("new-task")
 const task = document.getElementById("task")
 const dueDate = document.getElementById("due-date")
 const modal = document.getElementById("myModal");
-const tableList = document.getElementById("incomplete-tasks")
+const listOfTasks = document.getElementById("today-list");
+let tableList = document.getElementById('incomplete-tasks');
 
-// ---- Form validation -----
+
+/* ---------- Local storage ---------- */
+// Function to retrieve data and display data from local storage (if any)
+function getData() {
+	// Remove completed tasks
+	localStorage.removeItem("completed-tasks")
+	// Get incomplete tasks 
+	let previousTasks = JSON.parse(localStorage.getItem("incomplete-tasks"))
+	
+	// Insert into table if there are incomplete tasks
+	if (previousTasks == null) {
+		return
+	} else {
+		previousTasks.forEach((element) => {
+			tableList.insertAdjacentHTML('beforeEnd',
+				`<tr><td>${element.taskName}</td>
+				<td>${element.dueDate}</td>
+				<td class="checkboxes-col"><input type="checkbox"></td>
+				<td><button class="deleteButton-col">Delete</button></td></tr>`)
+		})
+	}
+}
+
+// Function to save new tasks to local storage
+function saveTDList() {
+	let incompleteTaskList = [];
+	let completedTaskList = [];
+
+	tableList.childNodes.forEach(function (item) {
+		if (item.tagName == "TR") {
+			let taskName = item.childNodes[0].innerText
+			let dueDate = item.childNodes[2].innerText
+			incompleteTaskList.push(new NewTask(taskName, dueDate))
+		}
+	});
+	document.getElementById('completed-tasks').childNodes.forEach(function (item) {
+		if (item.tagName == "TR") {
+			let taskName = item.childNodes[0].innerText
+			let dueDate = item.childNodes[2].innerText
+			completedTaskList.push(new NewTask(taskName, dueDate))
+		}
+	});
+
+	localStorage.setItem("incomplete-tasks", JSON.stringify(incompleteTaskList));
+	localStorage.setItem("completed-tasks", JSON.stringify(completedTaskList));
+}
+
+// Get data from local storage
+
+getData()
+
+
+/* ---------- Form Validation ---------- */
+
 // Validate length of task
 const validLength = (input, min) => {
 	if (input.value.trim().length >= min) {
@@ -30,12 +75,13 @@ const validLength = (input, min) => {
 	return input.validity.valid;
 }
 
+
 // Validate due date
 const validDate = (inputDate) => {
 	const currentDate = new Date();
 
 	const dueDate = new Date(inputDate.value + 'T00:00')
-	if (dueDate > currentDate) {
+	if (dueDate >= currentDate) {
 		inputDate.parentElement.classList.remove("invalid");
 		inputDate.validity.valid = true;
 		inputDate.setCustomValidity("");
@@ -52,48 +98,7 @@ const validDate = (inputDate) => {
 task.addEventListener("change", () => validLength(task, 1))
 dueDate.addEventListener("change", () => validDate(dueDate))
 
-// Add new task to table
-function addTaskToTable(task, dueDate) {
-	// Create new row with cell elements for task, dudedate and checkbox 
-	const newRow = document.createElement("tr")
-	const colTask = document.createElement("td")
-	const colDueDate = document.createElement("td")
-	const colCheckBox = document.createElement("td")
-	const colDelete = document.createElement("td")
-
-	// Add value to each element
-	colTask.textContent = task.value
-	colDueDate.textContent = dueDate.value
-
-	// Add Checkbox
-	const newCheckbox = document.createElement("input")
-	newCheckbox.type = "checkbox"
-	colCheckBox.appendChild(newCheckbox)
-	colCheckBox.classList.add("checkboxes-col")
-	
-	// Add Delete button
-	const delBtn = document.createElement("button")
-	delBtn.innerHTML = "Delete";
-	delBtn.classList.add("deleteButton-col")
-	colDelete.appendChild(delBtn)
-
-	// Append to new row and current table
-	newRow.append(colTask, colDueDate, colCheckBox, colDelete)
-	tableList.append(newRow)
-
-	// Clear value
-	task.value = ""
-	dueDate.value = ""
-}
-
-// Delete tasks button 
-document.addEventListener('click', (e) => {
-	if (e.target.classList.contains("deleteButton-col"))
-		e.target.closest("tr").remove()
-	saveTDList()
-})
-
-
+ 
 // Form validation for length and duedate
 const formValidation = (e) => {
 	e.preventDefault();
@@ -104,26 +109,61 @@ const formValidation = (e) => {
 	if (!validDate(dueDate)) {
 		valid = false;
 	}
-	return valid
+	return valid;
 }
 
 
-// Event listener for form validation
+/* ---------- Display to Table ---------- */
+// Class to create new tasks
+class NewTask {
+	constructor(task, dueDate){
+		this.taskName = task;
+		this.dueDate = dueDate;
+	}
+}
+
+// Add new data to table
+function addTaskToTable(task, dueDate) {
+	tableList.insertAdjacentHTML('beforeEnd',
+		`<tr><td>${task.value}</td>
+	<td>${dueDate.value}</td>
+	<td class="checkboxes-col"><input type="checkbox"></td>
+	<td><button class="deleteButton-col">Delete</button></td></tr>`)
+
+	// Clear value
+	task.value = "";
+	dueDate.value = "";
+}
+
+
+/* ---------- Event Listeners ---------- */
 form.addEventListener("submit", (e) => {
 
 	if (formValidation(e)) {
-		// Add to table: 
-		addTaskToTable(task, dueDate)
-		// Store to local storage ---- Need to change to proper format instead of HTML
-		saveTDList()
+		addTaskToTable(task, dueDate); 
+		saveTDList();
 	} else {
 		e.preventDefault();
 		form.reportValidity();
 	}
-
 })
 
+// Delete tasks button 
+document.addEventListener('click', (e) => {
+	if (e.target.classList.contains("deleteButton-col")) {
+		e.target.closest("tr").remove();
+		saveTDList();
+	}
+})
 
+// Reset Button
+document.getElementById("fresh-start").onclick = function () {
+	localStorage.clear()
+	location.reload()
+}
+
+
+/* ---------- Modal Image timeout ---------- */
 let imageTimeOut;
 // Display cat image as modal image
 function showCat(catUrl) {
@@ -131,42 +171,41 @@ function showCat(catUrl) {
 	modal.style.display = "block";
 	modalImg.src = catUrl;
 
+	// Remove image if not manually closed
 	imageTimeOut = setTimeout(() => {
 		modal.style.display = "none";
 	}, 5000)
 }
 
-// Get the <span> element that closes the modal
-var closeImage = document.getElementById("close");
-closeImage.onclick = function () {
+// Close image manually
+document.getElementById("close").onclick = function () {
 	modal.style.display = "none";
 	clearTimeout(imageTimeOut)
 }
 
-const listOfTasks = document.getElementById("today-list");
 
-// Fetch API to display cat image after checking a task
+/* ---------- Event Listener to fetch API ---------- */
 listOfTasks.addEventListener('change', (e) => {
-	let message = ""
+	let message = "";
 	// Pick message for completed vs uncompleted tasks 
 	if (e.target.checked) {
-		message = "good%20job%21"
+		message = "good%20job%21";
 
-		// Move marked checked!
-		const doneList = document.getElementById("completed-tasks")
-		doneList.appendChild(e.target.closest("tr"))
-		saveTDList()
+		// Move completed tasks to new table
+		const doneList = document.getElementById("completed-tasks");
+		doneList.appendChild(e.target.closest("tr"));
+		saveTDList();
 
 	} else {
-		message = "you%20got%20this%21"
-
-		const tableList = document.getElementById("incomplete-tasks")
-		tableList.appendChild(e.target.closest("tr"))
-		saveTDList()
+		message = "you%20got%20this%21";
+		// Return incomplete tasks to oiginal table
+		const tableList = document.getElementById("incomplete-tasks");
+		tableList.appendChild(e.target.closest("tr"));
+		saveTDList();
 	}
 
 	// Fetch API from Cataas
-	const url = `https://cataas.com/cat/says/${message}?font=Verdana&fontSize=30&fontColor=%23fff&position=bottom`
+	const url = `https://cataas.com/cat/says/${message}?font=Verdana&fontSize=30&fontColor=%23fff&position=bottom`;
 
 	fetch(url)
 		.then(function (data) {
@@ -175,13 +214,8 @@ listOfTasks.addEventListener('change', (e) => {
 			}
 			throw new Error('Unable to fetch cat image');
 		}).then(function (imageUrl) {
-			showCat(imageUrl)	
+			showCat(imageUrl)
 		}).catch((error) => {
 			console.log(error)
 		})
 });
-
-// doesn't take the final change
-// addEventListener("input", () => {
-// 	saveTDList()
-// })
